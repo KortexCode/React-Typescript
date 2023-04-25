2ecdd8a5461da36eabd66b5745d49ff0dbdf99bc
kortexcode <darkmaeno323@gmail.com>
2023-04-25 12:00:23 -0500

    Se agrega lógica para agregar imágenes de zorros con un botón, cambios al css, se tipa el estado images.

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,18 +1,11 @@
 
 type props = {
     url: string;
-    children: JSX.Element | JSX.Element[];
 }
 
-function RandomFox({url, children}: props ) : JSX.Element{
-    return (
-    
-        <figure>
-            <img src={url} alt="fox img"
-                width={320} height={"auto"} className="mt-3"/>
-            {children}
-        </figure>
-       
+function RandomFox({url}: props ) : JSX.Element{
+    return (    
+        <img src={url} alt="fox img" width={320} height={"auto"} className="mt-3"/>   
     )
 }
 export {RandomFox}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { RandomFox } from '@/components/RandomFox';
 import { useState } from 'react';
+import type {MouseEventHandler} from 'react';
 
 const randomNum = ()=> Math.floor(Math.random()*123 + 1);
 
 const randomId = ()=> Math.random().toString(36).substring(2, 9);
 
+//Ejemplo con types e interfaces
 type ApiFox = {
   id: string,
   url: string
@@ -18,19 +20,29 @@ export default function Home() {
     {id:randomId(), url:`https://randomfox.ca/images/${randomNum()}.jpg`},
   ]);
 
+  const handleAddFox: MouseEventHandler<HTMLButtonElement>  = () => {  
+    const newList: ApiFox[] = [...images];
+    const newImage: ApiFox = {id:randomId(), url:`https://randomfox.ca/images/${randomNum()}.jpg`};
+    newList.push(newImage);
+    setImages(newList);
+  }
+
   return (
-    <div>
+    
       <main>
-        <p>Chikorita Papu!!</p>
-        {images.map((item, index)=>
-          (<div key={index} className='my-2'>
-            <RandomFox url={item.url}>
-              <figcaption>Fox id: {item.id}</figcaption>
-            </RandomFox>
-          </div>) 
-        )}
+        <h1 className='fontSide-xl'>Chikorita Papu!!</h1>
+        <div className='py-2 my-4'>
+          <button className='ms-2 p-2 rounded-lg bg-red-300' onClick={handleAddFox}>Add new Fox</button>
+        </div>
+        <section className='w-full'>
+          {images.map((item, index)=>
+            (<div key={index} className='w-[80%] my-2 mx-auto grid grid-cols'>
+              <RandomFox url={item.url}/>
+            </div>) 
+          )}
+        </section>
       </main>
 
-    </div>
+  
   )
 }
